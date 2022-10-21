3fc8f61e830e3da4f80765f043ba7aa8311b8696
Srijan Shrestha <srijans318@gmail.com>
2022-10-21 17:08:03 -0400

    Working on footer component

--- src/Frontend/Component/Footer.tsx
+++ src/Frontend/Component/Footer.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import "../Style/footer.css"
 
 const Footer = () => {
     return (
-        <footer className="text-center p-2 fixed-bottom bg-dark text-white">
-            @Anorgeous {new Date().getFullYear()}
+        <footer className="p-4 fixed-bottom d-flex justify-content-between">
+            <div>Copyright©{new Date().getFullYear()} Annorgeous</div>
+            <div><i className="fa-solid fa-trash"></i>Barrie, Ontario, Canada</div>
         </footer>
     )
 }
