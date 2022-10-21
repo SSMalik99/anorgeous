8c177c31dbb81b678f4cab4bfa6ea4965f190400
Srijan Shrestha <srijans318@gmail.com>
2022-10-21 17:35:43 -0400

    Work on footer component

--- src/Frontend/Component/Footer.tsx
+++ src/Frontend/Component/Footer.tsx
@@ -5,7 +5,13 @@ const Footer = () => {
     return (
         <footer className="p-4 fixed-bottom d-flex justify-content-between">
             <div>Copyright©{new Date().getFullYear()} Annorgeous</div>
-            <div><i className="fa-solid fa-trash"></i>Barrie, Ontario, Canada</div>
+            <div><i className='uil uil-location-point'></i> Barrie, Ontario, Canada
+            <span className="px-2">|</span>
+            <span>
+                <i className='uil uil-linkedin px-2 icon'></i> 
+                <i className='uil uil-facebook px-2 icon'></i> 
+                <i className='uil uil-instagram px-2 icon'></i> 
+            </span></div>
         </footer>
     )
 }
