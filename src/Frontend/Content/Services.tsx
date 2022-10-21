cb72518ebf5a855ae7a7460efa226bf5aaf2b136
Srijan Shrestha <srijans318@gmail.com>
2022-10-21 18:23:27 -0400

    Work on service page

--- src/Frontend/Content/Services.tsx
+++ src/Frontend/Content/Services.tsx
@@ -1,12 +1,48 @@
 import React from "react";
-
+import "../Style/service.css"
 
 const Services = () => {
-    return (
-        <div className="container">
-            <h4>Services</h4>
-        </div>
-    )
-}
+  return (
+    <section>
+      <div className="container">
+        <h1 className="text-center">Our Services</h1>
+        <div className="mt-4 services d-flex flex-md-row flex-column">
+          <div className="card">
+            <div className="card-body text-center">
+              <i className="service-icon uil uil-programming-language"></i>
+              <h5 className="card-title">UI/UX & DESIGN</h5>
+              <p className="card-text mt-4">
+                Kickass designs, customized branding, and show stopper websites,
+                all designed to wow your customers.
+              </p>
+            </div>
+          </div>
 
-export default Services
+          <div className="card">
+            <div className="card-body text-center">
+              <i className="service-icon uil uil-programming-language"></i>
+              <h5 className="card-title">UI/UX & DESIGN</h5>
+              <p className="card-text mt-4">
+                Kickass designs, customized branding, and show stopper websites,
+                all designed to wow your customers.
+              </p>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-body text-center">
+              <i className="service-icon uil uil-programming-language"></i>
+              <h5 className="card-title">UI/UX & DESIGN</h5>
+              <p className="card-text mt-4">
+                Kickass designs, customized branding, and show stopper websites,
+                all designed to wow your customers.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
