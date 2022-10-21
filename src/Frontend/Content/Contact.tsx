0aa64bbe13e5d327e0528f4b9dbc09a830b1126e
Srijan Shrestha <srijans318@gmail.com>
2022-10-21 10:10:28 -0400

    Add contact form

--- src/Frontend/Content/Contact.tsx
+++ src/Frontend/Content/Contact.tsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from 'emailjs-com';
+
 
 const Contact = () => {
+    const form = useRef(null);
+
+    const sendEmail = (e:any) => {
+        e.preventDefault();
+
+        emailjs.sendForm('', '', JSON.stringify(form.current), '').then(
+            (result) => {
+                console.log(result.text);
+                alert('Email Sent Sucessfully!');
+            },
+            (error) => {
+                console.log(error.text);
+                alert('Email Sent Sucessfully!');
+            }
+        );
+
+        e.target.reset();
+    };
     return (
         <section>
             <div className="container">
             <h2>Contact Us</h2>
+                <form ref={form} onSubmit={sendEmail}>
                     <div className="row">
                         <div className="col-lg-8 col-12 mt-4">
                             <div className="row">
                                 <div className="col-lg-6 col-12 mb-3">
                                     <label htmlFor="fullName" className="form-label">Name</label>
-                                    <input type="text" className="form-control" id="fullName" placeholder="Full Name"/>
+                                    <input type="text" className="form-control" id="fullName" placeholder="Full Name" required/>
                                 </div>
                                 <div className="col-lg-6 col-12 mb-3">
                                     <label htmlFor="phone" className="form-label">Phone</label>
@@ -20,11 +41,11 @@ const Contact = () => {
                             <div className="row">
                                 <div className="mb-3">
                                     <label htmlFor="email" className="form-label">Email address</label>
-                                    <input type="email" className="form-control" id="email" placeholder="name@example.com"/>
+                                    <input type="email" className="form-control" id="email" placeholder="name@example.com" required/>
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="discussion" className="form-label">Example textarea</label>
-                                    <textarea className="form-control" id="discussion"></textarea>
+                                    <textarea className="form-control" id="discussion" required></textarea>
                                     </div>
                             </div>
                             <div className="d-flex justify-content-end">
@@ -32,6 +53,7 @@ const Contact = () => {
                             </div>
                         </div>
                     </div>
+                </form>
             </div>
         </section>
   
