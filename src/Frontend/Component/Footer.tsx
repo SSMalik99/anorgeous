3e1cd5ade9ea06d875535d8b68f51d084e3b91fd
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2022-10-21 21:56:34 -0400

    footer issue resolved

--- src/Frontend/Component/Footer.tsx
+++ src/Frontend/Component/Footer.tsx
@@ -1,19 +1,38 @@
 import React from "react";
-import "../Style/footer.css"
+import "../Style/footer.css";
 
 const Footer = () => {
-    return (
-        <footer className="p-4 fixed-bottom d-flex justify-content-between flex-column flex-sm-row">
-            <div>Copyright©{new Date().getFullYear()} Annorgeous</div>
-            <div><i className='uil uil-location-point'></i> Barrie, Ontario, Canada
-            <span className="px-2">|</span>
-            <span>
-                <i className='uil uil-linkedin px-2 icon'></i> 
-                <i className='uil uil-facebook px-2 icon'></i> 
-                <i className='uil uil-instagram px-2 icon'></i> 
-            </span></div>
-        </footer>
-    )
-}
+  return (
+    <footer className="p-4 d-flex justify-content-between flex-column flex-sm-row">
+      <div>Copyright©{new Date().getFullYear()} Annorgeous</div>
+      <div className="d-flex justify-content-between align-items-center">
+        <div className="">
+            <a 
+                href="#" className="text-decoration-none text-black h">
+                    {"Term & Conditions |  "} 
+            </a> 
+            
+            <a 
+                href="#" 
+                className="text-decoration-none text-black"> 
+                {" Privacy Policy | "} 
+                
+            </a>
 
-export default Footer
+            <a 
+                href="#" 
+                className="text-decoration-none text-black"> 
+                {" Site Map "}
+            </a>
+        </div>
+        {/* <span>
+          <i className="uil uil-linkedin px-2 icon"></i>
+          <i className="uil uil-facebook px-2 icon"></i>
+          <i className="uil uil-instagram px-2 icon"></i>
+        </span> */}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
