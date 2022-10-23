d928b9498dd8d56decd32488b9c3d0995f4f51d9
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2022-10-22 23:46:31 -0400

    product page updated

--- src/Frontend/Content/Work.tsx
+++ src/Frontend/Content/Work.tsx
@@ -9,29 +9,29 @@ const Work = () => {
         <div className="frontImageContainer">
             <div className="container work">
                 {/* Description and App Screenshot columns */}
-                <div className="row text-center">
+                <div className="row p-5">
 
-                    <div className="col-md-6">
-                    <h2 className="text-center mt-3">IAMOPEN</h2>
-                        <div>
-                            With IAMOPEN we aim to bring clarity in how
-                            to use the newest technological solutions 
-                            effectively and help avoid  their misuse 
-                            by providing  a thourough guidance from 
+                    <div className="col-md-6 left-content">
+                        <h2 className="left-above-content text-center mt-3">IAMOPEN</h2>
+                        <div className="left-below-content">
+                            With <span className="app_name_content"> IAMOPEN </span> we aim to bring clarity in how <br/>
+                            to use the newest technological solutions <br/>
+                            effectively and help avoid  their misuse <br/>
+                            by providing  a thourough guidance from <br/>
                             early buyers.
                         </div>
                     </div>
                     <div className="col-md-6">
-                        <div className="d-flex">
-                            <div className = "col-lg-6">
+                        <div className="row">
+                            <div className = "col-lg-6 pe-0">
                                 <img 
-                                    className="mt-2 mx-2 image-dimension"
+                                    className="mt-2 mx-0 image-dimension"
                                     src={iamopenSplash} 
                                     alt="Iamopen Splash Screen"/>
                             </div>
-                            <div className = "col-lg-6">
+                            <div className = "col-lg-6 p-2 pt-0 ps-0">
                                 <img
-                                    className="mt-2 mx-2 image-dimension"
+                                    className="mt-2 mx-1 image-dimension"
                                     src={iamopenMain} alt="Iamopen Main Page"></img>
                             </div>
                         </div>
